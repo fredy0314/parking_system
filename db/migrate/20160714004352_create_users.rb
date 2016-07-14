067aa9bf7b9f9312852a4771bc1301f370740437
fredy0314 <fredyloaiza6@gmail.com>
2016-07-13 19:57:13 -0500

    creacion de modelo usuario - vehiculo

---
+++ db/migrate/20160714004352_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :password
+      t.string :email
+      t.string :birthdate
+      t.string :date
+      t.boolean :is_admin
+
+      t.timestamps null: false
+    end
+  end
+end
